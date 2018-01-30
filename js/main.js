fcdff158db05a9289a546644a88d9ef8a53233f1
winggo <winggo.tse@gmail.com>
2018-01-30 00:26:37 -0800

    update self pic

---
+++ js/main.js
@@ -0,0 +1,5 @@
+var mod = require('../node_modules/pjax/index.js');
+
+new mod.Pjax({selectors: [".container"]})
+alert("works!");
+console.log("works!");
--- js/pjax.js
+++
@@ -1,2 +0,0 @@
-new Pjax({selectors: [".container"]})
-alert("works!");
