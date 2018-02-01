2cb7514ecff89113ed8ffc23003c2ee4631c07e6
winggo <winggo.tse@gmail.com>
2018-02-01 01:44:47 -0800

    test pjax implementation

---
+++ js/galleryScripts.js
@@ -0,0 +1,13 @@
+$("#mygallery").justifiedGallery({
+	rowHeight: 500,
+	margins: 5,
+	rel: 'gallery2'
+}).on('click', '[data-toggle="lightbox"]', function(event) {
+    event.preventDefault();
+    $(this).ekkoLightbox();
+});
+
+new Pjax({
+  selectors: [".container"]
+})
+alert("works!");
--- js/main.js
+++
@@ -1,5 +0,0 @@
-var mod = require('../node_modules/pjax/index.js');
-
-new mod.Pjax({selectors: [".container"]})
-alert("works!");
-console.log("works!");
